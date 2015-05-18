f7b83d0f7d165d4a349554defeee778338466b44
Draveness <stark.draven@gmail.com>
2015-05-18 11:12:30 +0800

    Initiliaze repo

---
+++ Classes/DKGradingView.h
@@ -0,0 +1,13 @@
+//
+//  DKGradingView.h
+//  DKGradingView
+//
+//  Created by apple on 15/5/18.
+//  Copyright (c) 2015年 DeltaX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKGradingView : UIView
+
+@end
---
+++ Classes/DKGradingView.m
@@ -0,0 +1,21 @@
+//
+//  DKGradingView.m
+//  DKGradingView
+//
+//  Created by apple on 15/5/18.
+//  Copyright (c) 2015年 DeltaX. All rights reserved.
+//
+
+#import "DKGradingView.h"
+
+@implementation DKGradingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
